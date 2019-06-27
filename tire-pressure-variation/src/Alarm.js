3b164b3a1793009bc25c008b0ad5d3bae6975a51
trikitrok <trikitrok@gmail.com>
2019-06-27 06:33:32 +0200

    making alarmOn property private

--- tire-pressure-variation/src/Alarm.js
+++ tire-pressure-variation/src/Alarm.js
@@ -1,24 +1,26 @@
 import Sensor from "./Sensor";
 
+var alarmOn = Symbol();
+
 export default class Alarm {
   constructor() {
     this.lowPressureThreshold = 17;
     this.highPressureThreshold = 21;
     this.sensor = new Sensor();
-    this.alarmOn = false;
+    this[alarmOn] = false;
   }
 
   check() {
     const psiPressureValue = this.sensor.popNextPressurePsiValue();
 
     if (psiPressureValue < this.lowPressureThreshold || this.highPressureThreshold < psiPressureValue) {
-      if(!this.alarmOn) {
-        this.alarmOn = true;
+      if(!this[alarmOn]) {
+        this[alarmOn] = true;
         console.log("Alarm activated!");
       }
     } else {
-      if(this.alarmOn) {
-        this.alarmOn = false;
+      if(this[alarmOn]) {
+        this[alarmOn] = false;
         console.log("Alarm deactivated!");
       }
     }
